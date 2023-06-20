613dfe5c5fc436282f504632e1781040b3079f1c
angcyo <angcyo@126.com>
2023-06-20 10:07:35 +0800

    fix json

--- UICoreDemo/script.debug.js
+++ UICoreDemo/script.debug.js
@@ -1,6 +1,6 @@
 //AppJs.T.showQQ("脚本调试中...");
 //AppJs.dialog.message("提示", "设备ID~~~~:" + AppJs.androidId);
 
-AppJs.sleep(160);
-AppJs.showReceiveCode();
-AppJs.hideScriptRunTipDialog();
+//AppJs.sleep(160);
+//AppJs.showReceiveCode();
+//AppJs.hideScriptRunTipDialog();
